6a315cb2dfe8b2642c25cc1c4e8a644b564288e7
Aki Aoki <akiko.aoki.0w0@gmail.com>
2022-07-30 19:20:56 +0400

    Version sync

--- Assets/Scripts/Controllers/BalanceController.cs
+++ Assets/Scripts/Controllers/BalanceController.cs
@@ -10,11 +10,14 @@ namespace Controllers
         public static float TicketBonus = 1.0f;
         public static float TicketBonusLevelMultiplier = 1.5f;
 
-        public static float TicketSpawnRate = 0.1f;
-        public static float TicketSpawnRateLevelMultiplier = 0.3f;
+        public static float TicketSpawnRate = 0.2f;
+        public static float TicketSpawnRateLevelMultiplier = 0.4f;
 
         public static float TicketMovementSpeed = 70.0f;
         public static float TicketMovementSpeedLevelMultiplier = 0.2f;
+
+        public static int LevelUpPeopleRequired = 1;
+        public static float LevelUpPeopleRequiredMultiplier = 2.0f;
 
         public static float GetTicketBonus(int level)
         {
@@ -30,6 +33,10 @@ namespace Controllers
         {
             return TicketMovementSpeed + TicketMovementSpeed * level * TicketMovementSpeedLevelMultiplier;
         }
-        
+
+        public static float GetLevelUpPeopleRequired(int level)
+        {
+            return LevelUpPeopleRequired + level * LevelUpPeopleRequiredMultiplier;
+        }
     }
 }
