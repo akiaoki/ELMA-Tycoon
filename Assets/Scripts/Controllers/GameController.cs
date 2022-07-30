d1f3ca5678ade3b82252b3146abd99f4b751d936
Aki Aoki <akiko.aoki.0w0@gmail.com>
2022-07-30 12:05:57 +0400

    Added controllers

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ELMA.SDK.Models;
+using UnityEngine;
+
+namespace Controllers
+{
+    public class GameController : MonoBehaviour
+    {
+
+        public UserModel UserModel { get; private set; }
+
+        private ELMAController _elmaController;
+        private BalanceController _balanceController;
+        private BuildController _buildController;
+        
+        private void Awake()
+        {
+            _elmaController = FindObjectOfType<ELMAController>();
+            _balanceController = FindObjectOfType<BalanceController>();
+            _buildController = FindObjectOfType<BuildController>();
+        }
+
+        public void Start()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/ELMA.SDK/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ELMA.SDK.Models
+{
+    public class UserModel
+    {
+        public string CompanyName;
+        public int Money;
+        public int Level;
+    }
+}
