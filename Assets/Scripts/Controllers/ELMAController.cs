21e20d7dda3b63ece575bc31ede59d07a779588f
Anton Zaviyalov <antonzaviyalov@gmail.com>
2022-07-30 20:27:53 +0300

    add edit user method

--- Assets/Scripts/Controllers/ELMAController.cs
+++ Assets/Scripts/Controllers/ELMAController.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Cache;
+﻿using System.Collections.Generic;
+using System.Net.Cache;
 using UnityEngine;
 using ELMA.SDK.Models;
 using System.Collections;
@@ -98,6 +99,29 @@ public class ELMAController : MonoBehaviour
             Debug.Log(req.downloadHandler.text);
             var resPayload = JsonConvert.DeserializeObject<UserModel>(req.downloadHandler.text);
             result(resPayload);
+        }
+    }
+
+        public IEnumerator UpdateUser(UserModel user, Action<bool> success) 
+    {
+        var url = "https://radvlfsvyzgxs.t-elma365.ru/api/extensions/7b2dc10b-1758-4b01-a74b-2b1a7622ccd7/script/editDataPlayer";
+        string jsonData = JsonConvert.SerializeObject(user);
+
+        var req = new UnityWebRequest(url, "POST");
+        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(jsonData);
+        req.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+        req.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        req.SetRequestHeader("Content-Type", "application/json");
+
+        yield return req.SendWebRequest();
+
+        if (req.responseCode != 204)
+        {
+            success(false);
+        }
+        else
+        {
+            success(true);
         }
     }
 }
