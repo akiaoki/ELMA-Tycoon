5e8e80372313ab283be570cea80c9448eb401edc
Anton Zaviyalov <antonzaviyalov@gmail.com>
2022-07-30 17:04:00 +0300

    add user create

--- Assets/Scripts/Controllers/ELMAController.cs
+++ Assets/Scripts/Controllers/ELMAController.cs
@@ -1,18 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using ELMA.SDK.Models;
+using System.Collections;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
 
 public class ELMAController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // Вызывать через StartCoroutine(CreateUser(user));
+    IEnumerator CreateUser(UserModel user) {
+        var url = "https://radvlfsvyzgxs.t-elma365.ru/api/extensions/7b2dc10b-1758-4b01-a74b-2b1a7622ccd7/script/createPlayer";
+        string jsonData = JsonConvert.SerializeObject(user);
+
+        var req = new UnityWebRequest(url, "POST");
+        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(jsonData);
+        req.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+        req.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        req.SetRequestHeader("Content-Type", "application/json");
+
+        yield return req.SendWebRequest();
     }
 }
--- Assets/Scripts/ELMA.SDK/Models/UserModel.cs
+++ Assets/Scripts/ELMA.SDK/Models/UserModel.cs
@@ -2,15 +2,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Newtonsoft.Json;
 
 namespace ELMA.SDK.Models
 {
     public class UserModel
     {
+        [JsonProperty("idElma")]
         public string IdElma;
+        [JsonProperty("nickName")]
         public string Nickname;
+        [JsonProperty("companyName")]
         public string CompanyName;
+        [JsonProperty("money")]
         public float Money;
+        [JsonProperty("level")]
         public int Level;
     }
 }
